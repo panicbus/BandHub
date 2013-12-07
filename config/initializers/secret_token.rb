1e484abf29eb7636345061ec1ca9b7ea61a8fdb9
Nico Crisafulli <elementalair@gmail.com>
2013-12-07 10:37:35 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bandhub::Application.config.secret_token = 'c1286c6f17b2bd7ca0f6ba354a37e1b07647b8eb2b6f3409bae79ab7c407984500fefbe35ed4d5fe49bb9cd74f3b7fa59748d426b2fdc749bc9059040b48f446'
