87a4c2b29350073a9cabcbbafe53256728de9e5b
panicbus <elementalair@gmail.com>
2015-03-28 12:51:46 -0700

    fixed horizontal scroll issue when pageslide open

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,20 +22,6 @@ $.ajaxSetup({
   }
 });
 
-
-// Slidemenu
-$(function() {
-  var $body = document.body
-  , $menu_trigger = $body.getElementsByClassName('menu-trigger')[0];
-
-  if ( typeof $menu_trigger !== 'undefined' ) {
-    $menu_trigger.addEventListener('click', function() {
-      $body.className = ( $body.className == 'menu-active' )? '' : 'menu-active';
-    });
-  }
-}).call(this);
-
-
 // initialize the ajax loading spinner
 $(function(){
   var $loading = $('#loading').hide();
